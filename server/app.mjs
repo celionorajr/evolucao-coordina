56bc7da294fbf45ccd5f98e2f73704fae7f7aba5
celionorajr <cnoraj@gmail.com>
2025-12-03 19:40:48 -0300

    att port

--- server/app.mjs
+++ server/app.mjs
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8083;
 
 // Middlewares
 app.use(express.json({ limit: '10mb' }));
